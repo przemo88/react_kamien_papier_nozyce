c65cbd5114b24c6593061952a433fe3901c4da48
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-12-27 23:07:29 +0100

    Updates

---
+++ precache-manifest.8866ffc4db5d51a90d04aa77e16f3415.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90cb731fd5e729e98ef316781a571a41",
+    "url": "przemo88/react_kamien_papier_nozyce/index.html"
+  },
+  {
+    "revision": "3a06d425de2f73081204",
+    "url": "przemo88/react_kamien_papier_nozyce/static/css/main.7eb150d7.chunk.css"
+  },
+  {
+    "revision": "1b76528c269e8559244a",
+    "url": "przemo88/react_kamien_papier_nozyce/static/js/2.ff0ed154.chunk.js"
+  },
+  {
+    "revision": "3a06d425de2f73081204",
+    "url": "przemo88/react_kamien_papier_nozyce/static/js/main.e3bcdb42.chunk.js"
+  },
+  {
+    "revision": "82e8e059262fdd035dcd",
+    "url": "przemo88/react_kamien_papier_nozyce/static/js/runtime-main.0a92c478.js"
+  }
+]);
