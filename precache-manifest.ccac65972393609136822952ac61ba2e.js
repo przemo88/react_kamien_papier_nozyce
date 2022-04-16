f3edcc5ba49d35d5a05d2de18c9c4d5dceade135
przemo88 <blagoff88@gmail.com>
2022-04-16 10:14:31 +0200

    Updates

---
+++ precache-manifest.ccac65972393609136822952ac61ba2e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6b570f48617c0682b95fcccd67e821",
+    "url": "/react_kamien_papier_nozyce/index.html"
+  },
+  {
+    "revision": "604824a46ccc4cc7ec0d",
+    "url": "/react_kamien_papier_nozyce/static/css/2.a97b732a.chunk.css"
+  },
+  {
+    "revision": "2e1fe5bc08b4f517c62d",
+    "url": "/react_kamien_papier_nozyce/static/css/main.90405171.chunk.css"
+  },
+  {
+    "revision": "604824a46ccc4cc7ec0d",
+    "url": "/react_kamien_papier_nozyce/static/js/2.aa629cfb.chunk.js"
+  },
+  {
+    "revision": "2e1fe5bc08b4f517c62d",
+    "url": "/react_kamien_papier_nozyce/static/js/main.e518ea7d.chunk.js"
+  },
+  {
+    "revision": "2ad1e2d73ac124ec0acc",
+    "url": "/react_kamien_papier_nozyce/static/js/runtime-main.bfbbb84c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react_kamien_papier_nozyce/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react_kamien_papier_nozyce/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react_kamien_papier_nozyce/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react_kamien_papier_nozyce/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react_kamien_papier_nozyce/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "przemo88/react_kamien_papier_nozyce/precache-manifest.8866ffc4db5d51a90d04aa77e16f3415.js"
+  "/react_kamien_papier_nozyce/precache-manifest.ccac65972393609136822952ac61ba2e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("przemo88/react_kamien_papier_nozyce/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_kamien_papier_nozyce/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
